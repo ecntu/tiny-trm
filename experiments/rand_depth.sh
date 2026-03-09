e18508ad83acd28b6273280988ee18a78934913f
Emilio Cantú <39497904+ecntu@users.noreply.github.com>
2026-03-08 20:58:51 -0400

    del k_passes

--- experiments/rand_depth.sh
+++ experiments/rand_depth.sh
@@ -7,19 +7,19 @@ mkdir -p models/rand_depth
 for seed in 1 2 3; do
 
     uv run main.py --batch_size 128 --h_dim 256 --N_sup 8 --halt_loss_weight 0.0 --halt_prob_thresh 2.0 \
-      --steps 12_500 --test_size 8192 --N_sup_test 256 --k_passes "$k_passes" \
+      --steps 12_500 --test_size 8192 --N_sup_test 256 \
       --init_state random --rand_n \
       --checkpoint "models/rand_depth/small_n_$seed.pt" \
       --seed "$seed"
 
     uv run main.py --batch_size 128 --h_dim 256 --N_sup 8 --halt_loss_weight 0.0 --halt_prob_thresh 2.0 \
-      --steps 12_500 --test_size 8192 --N_sup_test 256 --k_passes "$k_passes" \
+      --steps 12_500 --test_size 8192 --N_sup_test 256 \
       --init_state random --rand_T \
       --checkpoint "models/rand_depth/small_T_$seed.pt" \
       --seed "$seed"
 
       uv run main.py --batch_size 128 --h_dim 256 --N_sup 8 --halt_loss_weight 0.0 --halt_prob_thresh 2.0 \
-      --steps 12_500 --test_size 8192 --N_sup_test 256 --k_passes "$k_passes" \
+      --steps 12_500 --test_size 8192 --N_sup_test 256 \
       --init_state random --rand_n --rand_T \
       --checkpoint "models/rand_depth/small_n-T_$seed.pt" \
       --seed "$seed"
