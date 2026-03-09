6a183694a27f2ee8218c07cfaea1ed3d0653899d
Emilio Cantú <39497904+ecntu@users.noreply.github.com>
2026-03-08 20:57:17 -0400

    add

---
+++ experiments/rand_depth.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Try random depth idea with n, T and both
+
+mkdir -p models/rand_depth
+
+for seed in 1 2 3; do
+
+    uv run main.py --batch_size 128 --h_dim 256 --N_sup 8 --halt_loss_weight 0.0 --halt_prob_thresh 2.0 \
+      --steps 12_500 --test_size 8192 --N_sup_test 256 --k_passes "$k_passes" \
+      --init_state random --rand_n \
+      --checkpoint "models/rand_depth/small_n_$seed.pt" \
+      --seed "$seed"
+
+    uv run main.py --batch_size 128 --h_dim 256 --N_sup 8 --halt_loss_weight 0.0 --halt_prob_thresh 2.0 \
+      --steps 12_500 --test_size 8192 --N_sup_test 256 --k_passes "$k_passes" \
+      --init_state random --rand_T \
+      --checkpoint "models/rand_depth/small_T_$seed.pt" \
+      --seed "$seed"
+
+      uv run main.py --batch_size 128 --h_dim 256 --N_sup 8 --halt_loss_weight 0.0 --halt_prob_thresh 2.0 \
+      --steps 12_500 --test_size 8192 --N_sup_test 256 --k_passes "$k_passes" \
+      --init_state random --rand_n --rand_T \
+      --checkpoint "models/rand_depth/small_n-T_$seed.pt" \
+      --seed "$seed"
+  done
+done
