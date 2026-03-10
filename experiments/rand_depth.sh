57b2af4672e39da0a895d752fb6d4b56afe30314
Emilio Cantú <39497904+ecntu@users.noreply.github.com>
2026-03-10 08:58:19 -0400

    add baselines

--- experiments/rand_depth.sh
+++ experiments/rand_depth.sh
@@ -8,6 +8,17 @@ for seed in 1 2 3; do
 
     uv run main.py --batch_size 128 --h_dim 256 --N_sup 8 --halt_loss_weight 0.0 --halt_prob_thresh 2.0 \
       --steps 12_500 --test_size 8192 --N_sup_test 256 \
+      --checkpoint "models/rand_depth/small_baseline_$seed.pt" \
+      --seed "$seed"
+
+    uv run main.py --batch_size 128 --h_dim 256 --N_sup 8 --halt_loss_weight 0.0 --halt_prob_thresh 2.0 \
+      --steps 12_500 --test_size 8192 --N_sup_test 256 \
+      --init_state random \
+      --checkpoint "models/rand_depth/small_only-init_$seed.pt" \
+      --seed "$seed"
+
+    uv run main.py --batch_size 128 --h_dim 256 --N_sup 8 --halt_loss_weight 0.0 --halt_prob_thresh 2.0 \
+      --steps 12_500 --test_size 8192 --N_sup_test 256 \
       --init_state random --rand_n \
       --checkpoint "models/rand_depth/small_n_$seed.pt" \
       --seed "$seed"
